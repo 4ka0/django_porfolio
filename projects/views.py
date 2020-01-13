9ac31934dc8ec4953dd3eda0243a8d197642343b
Jon Lee <Jon@4ka0-laptop.fritz.box>
2020-01-13 22:45:22 +0000

    General update

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# each function here represents a view
+
+def project_list(request):
+    return render(request, 'projects/index.html')
