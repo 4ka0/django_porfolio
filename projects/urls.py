5c329173670203f6ced471920661fd11d731598f
Jon Lee <jage@fastmail.com>
2020-01-27 22:08:29 +0000

    Adding function to display an individual project

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,11 @@ from projects import views
 
 urlpatterns = [
     path('', views.all_projects),  # points to site/projects
+    path('<int:pk>', views.project_detail)  # '<int:pk>' gets an integer 
+                                            # representing the primary key for 
+                                            # an object in the database
+                                            # (only an int is accepted)
+                                            # the first param is passed to 
+                                            # the second param, basically
     # path('test', views.project_list),  # points to site/projects/test
 ]
--- projects/views.py
+++ projects/views.py
@@ -11,8 +11,24 @@ def project_list(request):
 '''
 
 def all_projects(request):
+    '''
+    Function to return all projects in the database.
+    '''
     # query database to return all project objects stored therein
     projects = Project.objects.all()
-    # pass the projects object to the template using a dict as a third argument
-    return render(request, 'projects/all_projects.html',
-                  {'projects': projects})
+    # param 1 = request
+    # param 2 = the template to be used
+    # param 3 = all project objects as a dict
+    return render(request, 'projects/all_projects.html', {'projects': projects})
+
+def project_detail(request, pk):
+    '''
+    Function to return the details of a single project.
+    '''
+    # retrieve the object having the primary key in question
+    project = Project.objects.get(pk=pk)
+    # param 1 = request
+    # param 2 = the template to be used
+    # param 3 = the individual project in question
+    return render(request, 'projects/detail.html', {'project': project})
+    
