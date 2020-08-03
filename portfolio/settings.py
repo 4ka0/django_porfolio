7d9ee5288be7b29a5857598809485f1e9ca6039d
Jon Lee <jage@fastmail.com>
2020-08-03 16:03:00 +0100

    Updated images

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-^^dv((5l#$55%#-*^6mm5eqi82b)xltsl)5w!7i=og&=y)=n='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
